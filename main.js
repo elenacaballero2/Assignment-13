1f220e8f484a8824be9c4c19e79d73c7011384a4
Elena <ecaballero2@fordham.edu>
2018-11-29 13:42:47 -0500

    track.search

--- main.js
+++ main.js
@@ -3,31 +3,33 @@ $(document).ready(function(){
 	$("#input").keypress(function(event){
 		if(event.keyCode == 13){
 			var searchTerm = $("#input").val();
-			$("h1").remove();
+			$("h3").remove();
 			$.ajax({
-				'url':"https://api.musixmatch.com/ws/1.1/artist.search?",
+				'url':"https://api.musixmatch.com/ws/1.1/track.search?",
 				'method': "GET",
 				'dataType': "JSONP",
 				'data':{
 					'apikey':"3cded554eb33d306f358f7fdce2ac783",
 					'q_artist': searchTerm,
+					'q_track': searchTerm,
 					'format': 'JSONP'
 				},
 
 				success:function(data){
 					console.log(data);
-					var mySongs = artist.search;
+					var mySongs = data.message.body.track_list;
 					for(var i=0; i<mySongs.length; i++){
-						if(artist.search[i] != searchTerm){
-							var newHeader = $("<h1>" + artist.search[i] + "</h1>");
+						if(mySongs[i].track.track_name != searchTerm){
+							var newHeader = $("<h3>" + mySongs[i].track.track_name + "</h3>");
 							$('body').append(newHeader);
 						}
 					}
+
 				},
 				error: function(data, textStatus, errorThrown){
 					console.log("oops")
 					console.log(errorThrown);
-				}
+				},
 
 			});
 		}
